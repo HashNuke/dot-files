5a8b782f7fdacc87fd61eacb9cba81160bcc1a80
Akash Manohar J <akash@akash.im>
2015-05-10 22:03:57 +0530

    Fix install script

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ ln -s $HOME/projects/dot-files/.vimrc $HOME/.vimrc
 ln -s $HOME/projects/dot-files/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/projects/dot-files/.gitconfig $HOME/.gitconfig
 ln -s $HOME/projects/dot-files/.gitignore $HOME/.gitignore_global
-echo "source $HOME/projects/dot-files/git-prompt.sh" >> ~/.bash_profile
+echo "source $HOME/projects/dot-files/term-config.sh" >> ~/.bash_profile
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
