ac7fe665d0d5a442d9c61c9619d779ca79310da5
Akash Manohar J <akash@akash.im>
2015-05-26 19:20:05 +0530

    emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,7 @@
+(setq package-archives
+      `(("gnu" . "http://elpa.gnu.org/packages")
+	("marmalage" . "http://marmalde-repo.org/packages/")
+	("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(ido-mode)
+(setq auto-save-default nil)
