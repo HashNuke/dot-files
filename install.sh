8ad2eb6816af2bdc6d53720b6fd7785a90a0d8aa
Akash Manohar J <akash@akash.im>
2015-08-14 15:43:43 +0530

    Add setup

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ ln -s $HOME/projects/dot-files/.gitignore $HOME/.gitignore_global
 ln -s $HOME/projects/dot-files/.emacs.d $HOME/.emacs.d
 printf "\nsource $HOME/projects/dot-files/term-config.sh" >> ~/.bash_profile
 
+sudo printf "\n127.0.0.1 rails.dev\n127.0.0.1 ember.dev\n127.0.0.1 phoenix.dev\n127.0.0.1 jekyll.dev" >> /etc/hosts
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 printf "\ninclude $HOME/projects/dot-files/nginx-servers/*;" >> /usr/local/etc/nginx/nginx.conf
