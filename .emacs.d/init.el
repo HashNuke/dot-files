fe997806c07e60e8af50561c5f4b3f424e52337e
Akash Manohar J <akash@akash.im>
2015-10-19 10:57:31 +0530

    Set theme only for GUI

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,10 @@
 
 ;;;;;;;;;;;;;;;; GLOBAL CONFIG ;;;;;;;;;;;;;;;;
 
+
+(set-face-attribute 'default nil :font  "Monaco 14")
+(set-frame-font "Monaco 16" nil t)
+
 (ido-mode 1)
 (setq ido-everywhere 1)
 (setq ido-enable-flex-matching t)
@@ -171,3 +175,19 @@
            :hline (concat alignment "|")
            :lstart "| " :lend " |" :sep " | ")))
     (orgtbl-to-generic table (org-combine-plists params2 params))))
+
+
+(when (display-graphic-p)
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(custom-enabled-themes (quote (tango-dark))))
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   )
+  )
