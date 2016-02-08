81fd15fa0cfaa31f6112d5de825cba803f949f31
Akash Manohar J <akash@akash.im>
2016-02-08 11:01:16 +0530

    Enable PS1 for mac too

--- term-config.sh
+++ term-config.sh
@@ -1,10 +1,12 @@
 source $HOME/projects/dot-files/git-prompt.sh
 
+# export TERM=xterm-256color
+
 export PATH=$HOME/bin:$PATH
 
 unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-  export PS1=$'\n\[\e[0;32m\]\w\[\e[1;31m\]$(__git_ps1 ":%s") \n\[\e[0;36m\]\xe2\x98\x85 \[\e[0m\]'  
+if [[ "$unamestr" == 'Linux' ]] || [[ "$unamestr" == 'Darwin' ]]; then
+  export PS1=$'\n\[\e[0;32m\]\w\[\e[1;31m\]$(__git_ps1 ":%s") \n\[\e[0;36m\]\xe2\x98\x85 \[\e[0m\]'
 # elif [[ "$unamestr" == 'FreeBSD' ]]; then
 fi
 
