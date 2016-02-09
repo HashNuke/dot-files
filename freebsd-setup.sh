aff349e2c076dc6aae40a83551a534137a754a7c
Akash Manohar <akash@akash.im>
2016-02-09 22:03:21 +0530

    Add java and gmake to the list

--- freebsd-setup.sh
+++ freebsd-setup.sh
@@ -1,6 +1,10 @@
 
-sudo pkg install -y bash vim ediors/emacs git tmux automake autoconf openssl libyaml readline libxslt libtool unixodbc
+sudo pkg install -y bash vim ediors/emacs git tmux gmake automake autoconf openssl libyaml readline libxslt libtool unixodbc
 
+sudo pkg install -y openjdk8
+
+
+sudo sh -c 'echo "proc  /proc   procfs rw  0 0" >> /etc/fstab'
 sudo sh -c 'echo "fdesc /dev/fd fdescfs rw 0 0" >> /etc/fstab'
 sudo mount -a
 
