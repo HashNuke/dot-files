f2fa5720fd86fc90cfaefa40bbea8e66c01ae0cb
Akash Manohar <akash@akash.im>
2016-02-10 10:36:23 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,15 @@ ln -s $HOME/projects/dot-files/.gitconfig $HOME/.gitconfig
 ln -s $HOME/projects/dot-files/.gitignore $HOME/.gitignore_global
 ln -s $HOME/projects/dot-files/.emacs.d $HOME/.emacs.d
 echo "source $HOME/projects/dot-files/term-config.sh" >> ~/.bash_profile
+echo "source $HOME/projects/dot-files/term-config.sh" >> ~/.bashrc
 
 sudo apt-get install -y automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
 sudo apt-get install -y openjdk-8-jdk
 
 git clone git@github.com:HashNuke/asdf.git $HOME/.asdf
+echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo '. $HOME/.asdf/asdf.sh' >> ~/.bash_profile
+source ~/.bash_profile
 asdf plugin-add erlang git@github.com:HashNuke/asdf-erlang.git
 asdf plugin-add elixir git@github.com:HashNuke/asdf-elixir.git
 asdf plugin-add nodejs git@github.com:HashNuke/asdf-nodejs.git
