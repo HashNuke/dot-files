14e585258e54bcaf456a2485bdd60f041b450094
Akash Manohar J <akash@akash.im>
2016-10-01 19:29:18 +0530

    Set zsh prompt

--- term-config.sh
+++ term-config.sh
@@ -24,3 +24,7 @@ alias gu="git pull"
 
 alias n=nano
 alias ia="open $1 -a /Applications/iA\ Writer\ Pro.app"
+
+PROMPT='
+%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}$(collapse_pwd) %{$reset_color%}$(hg_prompt_info)$(git_prompt_info)
+$(virtualenv_info)$(prompt_char) '
