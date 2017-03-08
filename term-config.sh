8414e7de8df682f70fa091897bf1190603e09593
Akash Manohar J <akash@akash.im>
2017-03-08 13:01:50 +0530

    GPG TTY

--- term-config.sh
+++ term-config.sh
@@ -67,3 +67,6 @@ ZSH_THEME_GIT_PROMPT_CLEAN=""
 PROMPT='
 %{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%}$(hg_prompt_info)$(git_prompt_info)
 $(virtualenv_info)$(prompt_char) '
+
+GPG_TTY=$(tty)
+export GPG_TTY
