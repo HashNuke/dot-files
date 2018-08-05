d018b73122cdb6f7db96a137287345319b4c0d49
Akash Manohar <akash@akash.im>
2018-08-05 06:46:06 +0530

    Add to things

--- add-to-things.rb
+++ add-to-things.rb
@@ -1,21 +1,26 @@
 require 'uri'
 
-project = "SomeProject"
+"
+Generates Things.app urls to add books as projects with chapters as tasks
+
+USAGE:
+ruby add-to-things.rb <name> <chapter-count>
+
+# For somebook with 5 chapters
+ruby add-to-things.rb SomeBook 5
+"
+
+project = ARGV[0]
 titles = (1..11).map {|i| "Chapter-#{i}"}
 
-units = [
-  {name: "Foo", count: 4},
-  {name: "Bar", count: 5}
-]
+puts "#{ARGV.inspect}"
+unit_count = (ARGV[1] || 3).to_i
 
-# titles = []
-# units.each do |unit|
-#   titles += (1..unit[:count]).map {|i| "#{unit[:name]}-#{i}" }
-# end
-
+titles = (1..unit_count).map {|i| "Chapter-#{i}"}
 
 titles = URI.encode titles.join("\n")
 project = URI.encode project
 
 things_url = "things:///add?titles=#{titles}&list=#{project}&reveal=true"
 puts things_url
+
