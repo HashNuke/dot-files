5e511be90dab77340c2f9ae89aad4afe857c32f3
Akash Manohar <akash@akash.im>
2018-08-09 12:20:47 +0530

    Support adding to existing list

--- add-to-things.rb
+++ add-to-things.rb
@@ -8,16 +8,34 @@ ruby add-to-things.rb <name> <chapter-count>
 
 # For somebook with 5 chapters
 ruby add-to-things.rb SomeBook 5
+
+LIST_ID=project-or-area-id ruby add-to-things.rb SomeBook 5
 "
 
-project = ARGV[0]
-unit_count = (ARGV[1] || 3).to_i
-todo_prefix = ARGV[2] || "Chapter"
+list_id = ENV['LIST_ID']
+arg_index = if list_id
+  {unit_count: 0, todo_prefix: 1, action_name: 'add'}
+else
+  {project: 0, unit_count: 1, todo_prefix: 2, action_name: 'add-project'}
+end
+
+project = if arg_index[:project]
+  ARGV[arg_index[:project]]
+end
+
+unit_count = (ARGV[arg_index[:unit_count]] || 3).to_i
+todo_prefix = ARGV[arg_index[:todo_prefix]] || "Chapter"
 
 titles = (1..unit_count).map {|i| "#{todo_prefix}-#{i}"}
-titles = URI.encode titles.join("\n")
-project = URI.encode project
+titles = URI.encode(titles.join("\n"))
 
-things_url = "things:///add-project?to-dos=#{titles}&title=#{project}&reveal=true"
+things_url = if ENV['LIST_ID']
+   list_id = URI.encode(list_id)
+  "things:///#{arg_index[:action_name]}?titles=#{titles}&list-id=#{list_id}&reveal=true"
+else
+  project = URI.encode(project)
+  "things:///#{arg_index[:action_name]}?to-dos=#{titles}&title=#{project}&reveal=true"
+end
+
 puts things_url
 
