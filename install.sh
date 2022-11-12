15687931976b6e781e3322cca0b835cc7a8f70f9
Akash Manohar <akash@akash.im>
2022-11-13 02:34:06 +0530

    Update install.sh with configurable projects dir

--- install.sh
+++ install.sh
@@ -1,31 +1,33 @@
 #/usr/bin/env bash
 
+export PROJECTS_DIR="code"
+
 mkdir -p ~/bin
 
 git clone git@github.com:HashNuke/dot-files.git
 
-ln -s $HOME/projects/dot-files/.vimrc $HOME/.vimrc
+ln -s $HOME/${PROJECTS_DIR}/dot-files/.vimrc $HOME/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-ln -s $HOME/projects/dot-files/.psqlrc $HOME/.psqlrc
-ln -s $HOME/projects/dot-files/.tmux.conf $HOME/.tmux.conf
-ln -s $HOME/projects/dot-files/bin/start-tmux $HOME/bin/start-tmux
-ln -s $HOME/projects/dot-files/bin/psqlc $HOME/bin/psqlc
+ln -s $HOME/${PROJECTS_DIR}/dot-files/.psqlrc $HOME/.psqlrc
+ln -s $HOME/${PROJECTS_DIR}/dot-files/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/${PROJECTS_DIR}/dot-files/bin/start-tmux $HOME/bin/start-tmux
+ln -s $HOME/${PROJECTS_DIR}/dot-files/bin/psqlc $HOME/bin/psqlc
 
 # Install oh-my-zsh
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Skip if linux; Use default theme
-ln -s $HOME/projects/dot-files/honukai.zsh-theme $HOME/.oh-my-zsh/themes/honukai.zsh-theme
+ln -s $HOME/${PROJECTS_DIR}/dot-files/honukai.zsh-theme $HOME/.oh-my-zsh/themes/honukai.zsh-theme
 
-ln -s $HOME/projects/dot-files/.gdbinit $HOME/.gdbinit
+ln -s $HOME/${PROJECTS_DIR}/dot-files/.gdbinit $HOME/.gdbinit
 touch $HOME/.gitconfig.local
-ln -s $HOME/projects/dot-files/.gitconfig $HOME/.gitconfig
-ln -s $HOME/projects/dot-files/.gitignore $HOME/.gitignore_global
-ln -s $HOME/projects/dot-files/.emacs.d $HOME/.emacs.d
-ln -s $HOME/projects/dot-files/.zshrc $HOME/.zshrc
+ln -s $HOME/${PROJECTS_DIR}/dot-files/.gitconfig $HOME/.gitconfig
+ln -s $HOME/${PROJECTS_DIR}/dot-files/.gitignore $HOME/.gitignore_global
+ln -s $HOME/${PROJECTS_DIR}/dot-files/.emacs.d $HOME/.emacs.d
+ln -s $HOME/${PROJECTS_DIR}/dot-files/.zshrc $HOME/.zshrc
 
-echo "source $HOME/projects/dot-files/term-config.sh" >> ~/.zshrc
+echo "source $HOME/${PROJECTS_DIR}/dot-files/term-config.sh" >> ~/.zshrc
 
 ## Ubuntu
 # sudo apt-get install -y zsh git-core make automake autoconf build-essential libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev unzip wget tshark
@@ -43,4 +45,4 @@ asdf plugin-add nodejs git@github.com:HashNuke/asdf-nodejs.git
 asdf plugin-add ruby git@github.com:HashNuke/asdf-ruby.git
 
 
-cp -R ~/projects/dot-files/scripts/Screencasting $HOME/Library/Scripts/Screencasting
+cp -R ~/${PROJECTS_DIR}/dot-files/scripts/Screencasting $HOME/Library/Scripts/Screencasting
