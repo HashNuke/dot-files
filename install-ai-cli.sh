860703cc6a3fd76f14af7e7e35358f55b9afd9db
Akash Manohar <84005+HashNuke@users.noreply.github.com>
2026-03-27 19:29:08 +0700

    Addd pusher script

--- install-ai-cli.sh
+++ install-ai-cli.sh
@@ -1,6 +1,5 @@
 npm install -g @openai/codex \
     @anthropic-ai/claude-code \
-    @github/copilot
-    # @google/gemini-cli \
-    # @charmland/crush \
-    # opencode-ai
+    @github/copilot \
+    @google/gemini-cli \
+    opencode-ai
